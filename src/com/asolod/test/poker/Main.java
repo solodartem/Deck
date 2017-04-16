e35e8c6e5abcacf0381414b792ad44f34d8c1e76
solodartem <solod_a@ukr.net>
2017-04-16 20:02:07 +0300

    Polishing

--- src/com/asolod/test/poker/Main.java
+++ src/com/asolod/test/poker/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("floorLayout.fxml"));
         Parent root = loader.load();
 
         primaryStage.setTitle("Poker test");
