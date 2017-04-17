f3f18f52cb8dc3b4b97a30d6842ecf9364eb943c
solodartem <solod_a@ukr.net>
2017-04-17 19:57:07 +0300

    Ant now works

--- createjar.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-javapackager -createjar -appclass package.ClassName \
-    -srcdir out/production \
-    -outdir package \
-    -outfile outjar -v
--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-javapackager -deploy -native \
--BsystemWide=true \
--BjvmOptions=-Xmx128m \
--BjvmOptions=-Xms128m \
--outdir packages \
--outfile PokerTest \
--srcdir ./src \
--appclass com.asolod.test.poker.Main
